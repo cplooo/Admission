2a3a4c5ec19802f4bf0d0be28cd29d5f9e01ac36
cplooo <cplo@kimo.com>
2024-05-29 17:50:55 +0800

    Add files via upload

--- Admission_3_streamlit_GitHub.py
+++ Admission_3_streamlit_GitHub.py
@@ -163,7 +163,7 @@ def load_data(path):
 
 ###### 計算次數分配並形成 包含'項目', '人數', '比例' 欄位的 dataframe 'result_df'
 @st.cache_data(ttl=3600, show_spinner="正在處理資料...")  ## Add the caching decorator
-def Frequency_Distribution(df, column_index, split_symbol='-', dropped_string='沒有工讀', sum_choice=1):
+def Frequency_Distribution(df, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1):
     ##### 将字符串按分割符號分割并展平
     split_values = df.iloc[:,column_index].str.split(split_symbol).explode()  ## split_symbol='-'
     #### split_values資料前處理
@@ -1008,6 +1008,47 @@ with st.expander("Q8. 認識與瞭解靜宜大學的管道:"):
     # df_admission.iloc[:,7] ## 
     column_index = 7
     item_name = "認識與瞭解靜宜大學的管道"
+    column_title.append(df_admission.columns[column_index][0:])
+    # set(df_admission_original['科系'])
+    rank_number = 5
+
+    ##### 產出 result_df
+    result_df = Frequency_Distribution(df_admission, column_index, split_symbol='\n', dropped_string='沒有工讀', sum_choice=1)    
+    # #### 選取前面 5 筆資料
+    # result_df = result_df.head(rank_number)
+    ##### 存到 list 'df_streamlit'
+    df_streamlit.append(result_df)  
+
+    ##### 使用Streamlit展示DataFrame "result_df"，但不显示索引
+    # st.write(choice)
+    st.write(f"<h6>{choice}</h6>", unsafe_allow_html=True)
+    st.write(result_df.to_html(index=False), unsafe_allow_html=True)
+    st.markdown("##")  ## 更大的间隔
+
+    ##### 使用Streamlit畫單一圖 & 比較圖
+    #### 畫比較圖時, 比較單位之選擇:
+    if 院_系 == '0':
+        ## 使用multiselect组件让用户进行多重选择
+        # selected_options = st.multiselect('選擇比較學系：', df_admission_original['科系'].unique(), default=[choice,'企業管理學系'],key=str(column_index)+'d')  ## # selected_options = ['化科系','企管系']
+        selected_options = st.multiselect('選擇比較學系：', departments_list, default=[choice,'企業管理學系'],key=str(column_index)+'d')  ## # selected_options = ['化科系','企管系']
+    if 院_系 == '1':
+        ## 使用multiselect组件让用户进行多重选择
+        # selected_options = st.multiselect('選擇比較學院：', df_admission_original['學院'].unique(), default=[choice,'資訊學院'],key=str(column_index)+'f')
+        selected_options = st.multiselect('選擇比較學院：', faculties_list, default=[choice,'資訊學院'],key=str(column_index)+'f')
+
+    # Draw(院_系, column_index, ';', '沒有工讀', 1, result_df, selected_options, dataframes, combined_df)
+    # Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df, selected_options)
+    Draw(院_系, column_index, split_symbol='\n', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=15,xlabel_fontsize = 14,ylabel_fontsize = 14,legend_fontsize = 14,xticklabel_fontsize = 14, yticklabel_fontsize = 14, annotation_fontsize = 14, bar_width = 0.2, fontsize_adjust=0, item_name=item_name, rank=False, rank_number=rank_number)    
+    
+st.markdown("##")  ## 更大的间隔 
+
+
+
+###### 一階篩選公告後是否有收到通過學系之聯絡
+with st.expander("Q10. 一階篩選公告後是否有收到通過學系之聯絡:"):
+    # df_admission.iloc[:,9] ## 
+    column_index = 9
+    item_name = "一階篩選公告後是否有收到通過學系之聯絡"
     column_title.append(df_admission.columns[column_index][0:])
     # set(df_admission_original['科系'])
     rank_number = 5
