332729b0f6f94f82ee966074388f88c58e7b0da2
cplooo <cplo@kimo.com>
2024-06-04 01:27:12 +0800

    Add files via upload

--- Admission_3_streamlit_GitHub.py
+++ Admission_3_streamlit_GitHub.py
@@ -1415,7 +1415,8 @@ with st.expander("Q6-特定高中. 特定高中參與靜宜大學申請入學主
     highlight_schools = ['清水高中','龍津高中','中港高中','弘文高中','新民高中','僑泰高中','立人高中']
     selected_options = st.multiselect('選擇重點高中：', highlight_schools, default=['清水高中','龍津高中'],key='highlight_schools_1')
     # item_name = f"{selected_options} 參與靜宜大學申請入學主要原因(複選)"
-    item_name = f"{', '.join(selected_options)} 參與靜宜大學申請入學主要原因(複選)"
+    # item_name = f"{', '.join(selected_options)} 參與靜宜大學申請入學主要原因(複選)"
+    item_name = "重點高中 V.S.一般高中 參與靜宜大學申請入學主要原因(複選)"
     
     ##### 產出 result_df: 加條件: 挑選出 selected_options 中的特定高中
     if 系_院_校 == '0':
