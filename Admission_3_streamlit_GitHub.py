3d746d37abae5e170484c34f7c882919c9b6179d
cplooo <cplo@kimo.com>
2024-08-07 18:31:11 +0800

    Add files via upload

--- Admission_3_streamlit_GitHub.py
+++ Admission_3_streamlit_GitHub.py
@@ -1794,7 +1794,7 @@ with st.expander("Q4. 各學系填答人數與比例(複選): 在所選擇群體
     rank_number = 5
 
     ##### 產出 result_df
-    result_df = Frequency_Distribution(df_admission, column_index, split_symbol='\n', dropped_string='沒有工讀', sum_choice=1)    
+    result_df = Frequency_Distribution(df_admission, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1)    
     # #### 選取前面 5 筆資料
     # result_df = result_df.head(rank_number)
     ##### 存到 list 'df_streamlit'
@@ -1823,7 +1823,7 @@ with st.expander("Q4. 各學系填答人數與比例(複選): 在所選擇群體
 
     # Draw(系_院_校, column_index, ';', '沒有工讀', 1, result_df, selected_options, dataframes, combined_df)
     # Draw(系_院_校, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df, selected_options)
-    Draw(系_院_校, column_index, split_symbol='\n', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=20,xlabel_fontsize = 18,ylabel_fontsize = 18,legend_fontsize = 18,xticklabel_fontsize = 18, yticklabel_fontsize = 13, annotation_fontsize = 16, bar_width = 0.1, fontsize_adjust=1, item_name=item_name, rank=False, rank_number=5, df_admission=df_admission, df_admission_faculty=df_admission_faculty)    
+    Draw(系_院_校, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=20,xlabel_fontsize = 18,ylabel_fontsize = 18,legend_fontsize = 18,xticklabel_fontsize = 18, yticklabel_fontsize = 13, annotation_fontsize = 16, bar_width = 0.1, fontsize_adjust=1, item_name=item_name, rank=False, rank_number=5, df_admission=df_admission, df_admission_faculty=df_admission_faculty)    
     plt.close()  # 關閉當前圖形以釋放內存
 st.markdown("##")  ## 更大的间隔  
 
